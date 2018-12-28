7b69827e786026364a1a63242736adec402686d3
jddelia <jddelia123@gmail.com>
2018-12-27 21:34:37 -0500

    Updates

---
+++ precache-manifest.c30959792a72eededcbf7fee3fce64f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1fe60ef306edd78df73",
+    "url": "/reactchords/static/js/runtime~main.a1fe60ef.js"
+  },
+  {
+    "revision": "0985341cae3f0e298721",
+    "url": "/reactchords/static/js/main.0985341c.chunk.js"
+  },
+  {
+    "revision": "2b88299bdd69279f4a5e",
+    "url": "/reactchords/static/js/1.2b88299b.chunk.js"
+  },
+  {
+    "revision": "0985341cae3f0e298721",
+    "url": "/reactchords/static/css/main.583d32c5.chunk.css"
+  },
+  {
+    "revision": "eb6fb4d26c0d37ab7708c0151763b06d",
+    "url": "/reactchords/index.html"
+  }
+];
