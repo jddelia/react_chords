46771eab510efc0c4119cc37f65344c6c64b8a12
jddelia <jddelia123@gmail.com>
2018-12-28 09:40:10 -0500

    Create express server, prep for heroku deploy

---
+++ build/precache-manifest.09d4323a6fce708dc3ae2892755b6215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38d4c71c350fe0405cb1",
+    "url": "/static/js/main.38d4c71c.chunk.js"
+  },
+  {
+    "revision": "2b88299bdd69279f4a5e",
+    "url": "/static/js/1.2b88299b.chunk.js"
+  },
+  {
+    "revision": "38d4c71c350fe0405cb1",
+    "url": "/static/css/main.583d32c5.chunk.css"
+  },
+  {
+    "revision": "4e283bf34ec6343609e316595a1552ff",
+    "url": "/index.html"
+  }
+];
